3bcd40b1a80ef848ab0247ad809055fe65522728
Emiliano <emiliano.bocanegra@utrgv.edu>
2023-11-27 16:46:37 -0600

    Sup

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
-print("hello")
-print("hello")
-print("HE")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run()
